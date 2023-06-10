f2a6ecec00fc08467851cf54114db0a0a0abedbd
JustWaveThings <justwavethings@proton.me>
2023-06-10 12:41:15 -0400

    Base functionality for catalog to render product. WIP

--- shopping-cart/src/components/Catalog.jsx
+++ shopping-cart/src/components/Catalog.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { Suspense } from 'react';
+import Product from './Product';
+import getProducts from '../api';
+import { useLoaderData, defer, Await } from 'react-router-dom';
+
+export function loader() {
+	return defer({
+		product: getProducts()
+	});
+}
 
 function Catalog() {
-  return <div className='catalog--cont'>Catalog</div>;
+	const loaderData = useLoaderData();
+
+	return (
+		<Suspense fallback={<h2>Loading Products... </h2>}>
+			<Await resolve={loaderData.product}>
+				<div className="catalog--cont">
+					<Product />
+					<Product />
+					<Product />
+				</div>
+			</Await>
+		</Suspense>
+	);
 }
 
 export default Catalog;
--- shopping-cart/src/components/Product.jsx
+++ shopping-cart/src/components/Product.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+
 function Product() {
-  return <div>Product</div>;
+  return <div className="product--cont">
+    <img src="favicon.ico" alt="test" className="product--cont--image"/>
+    <div className="product--cont--bottom">
+      <div className="product--cont--bottom--top">
+        <div className="product--name">Test Product</div>
+        <div className="product--price">$100</div>
+      </div>
+      <div className="product--cont--bottom--bottom">
+        <div className="product--qty--label">Qty:</div>
+        <button className="product--qty--increment">-</button>
+        <input className="product--qty--input" placeholder="0" type="number" min={0} />
+
+        <button className="product--qty--increment">+</button>
+
+      </div>
+      <button className="product--add--to--cart"> Add to Cart </button>
+    </div>
+  </div>;
 }
 
 export default Product;
