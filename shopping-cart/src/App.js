78668f332f3f7be1d2f70a1e6df392399f29cf37
JustWaveThings <justwavethings@proton.me>
2023-06-10 12:39:49 -0400

    Add necessary imports and loader attributes

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,32 +1,45 @@
-import React from "react";
+import React from 'react';
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
-import Layout from "./components/Layout.jsx";
-import Home from "./components/Home.jsx";
-import Catalog from "./components/Catalog.jsx";
-import Product from "./components/Product.jsx";
-import Cart from "./components/Cart.jsx";
-import NotFound from "./components/NotFound.jsx";
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider
+} from 'react-router-dom';
+import './index.css';
+import Layout from './components/Layout.jsx';
+import Home from './components/Home.jsx';
+import Catalog, { loader as productLoader } from './components/Catalog.jsx';
+import Cart from './components/Cart.jsx';
+import NotFound from './components/NotFound.jsx';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} />
-      <Route path="catalog" element={<Catalog />} />
-      <Route path="catalog/:id" element={<Product />} />
-      <Route path="cart" element={<Cart />} />
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
+	createRoutesFromElements(
+		<Route
+			path="/"
+			element={<Layout />}>
+			<Route
+				index
+				element={<Home />}
+			/>
+			<Route
+				path="catalog"
+				element={<Catalog />}
+				loader={productLoader}
+			/>
+			<Route
+				path="cart"
+				element={<Cart />}
+			/>
+			<Route
+				path="*"
+				element={<NotFound />}
+			/>
+		</Route>
+	)
 );
 
 function App() {
-  return <RouterProvider router={router}></RouterProvider>;
+	return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
