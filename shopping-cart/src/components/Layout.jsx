06093d508608534aa69774cb2f0edab3b3d62c4f
JustWaveThings <justwavethings@proton.me>
2023-06-18 14:21:46 -0400

    Linter updates

--- shopping-cart/src/components/Layout.jsx
+++ shopping-cart/src/components/Layout.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import Header from "./Header.jsx";
-import { Outlet } from "react-router-dom";
-import Footer from "./Footer.jsx";
+import React from 'react';
+import Header from './Header.jsx';
+import { Outlet } from 'react-router-dom';
+import Footer from './Footer.jsx';
 
 function Layout() {
-  return (
-    <>
-      <Header />
-      <Outlet />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<Outlet />
+			<Footer />
+		</>
+	);
 }
 
 export default Layout;
